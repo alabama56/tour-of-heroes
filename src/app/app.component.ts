fc2c6a7b222ba38a76d3465d0a2ed7334e75c5b4
alabama56 <jakedownard@yahoo.com>
2017-10-20 16:47:52 -0500

    using as a reference for chirper lab and future angulr labs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <nav>
+            <a routerLink="/dashboard" routerlinkActive="active">Dashboard</a>
+            <a routerLink="/heroes" routerlinkActive="active">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `,
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+    title = 'Tour of Heros';
 }
